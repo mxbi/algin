05c9ac7d6059f8e0146ebdf6f925d5135907cc49
Mikel Bober-Irizar <mikel@mxbi.net>
2016-08-21 13:54:54 +0200

    Expand vector operations to both scalars and vectors

--- vec.py
+++ vec.py
@@ -19,25 +19,62 @@ class v(object):
     def __eq__(self, ov):
         return self.coor == ov.coor
 
-    def plus(self, ov):
+    # Addition
+
+    def plus_vector(self, ov):
         assert self.dim == ov.dim, 'Vectors must be the same dimensionality.'
         return v([x + y for x, y in zip(self.coor, ov.coor)])
 
-    def __add__(self, ov):
-        return self.plus(ov)
+    def plus_scalar(self, c):
+        return v([x + c for x in self.coor])
 
-    def minus(self, ov):
+    def __add__(self, o):
+        if type(o) in [int, float]:
+            return self.plus_scalar(o)
+        elif type(o) is v:
+            return self.plus_vector(o)
+        else:
+            return NotImplemented
+
+    def __radd__(self, o):
+        return self.__add__(o)
+
+    # Subtraction
+
+    def minus_vector(self, ov):
         assert self.dim == ov.dim, 'Vectors must be the same dimensionality.'
         return v([x + y for x, y in zip(self.coor, ov.coor)])
 
-    def __sub__(self, ov):
-        return self.minus(ov)
+    def minus_scalar(self, c):
+        return v([x - c for x in self.coor])
+
+    def __sub__(self, o):
+        if type(o) in [int, float]:
+            return self.plus_scalar(o)
+        elif type(o) is v:
+            return self.plus_vector(o)
+        else:
+            return NotImplemented
+
+    def __rsub__(self, o):
+        return self.__sub__(o)
+
+    # Multiplication
 
     def times_scalar(self, c):
         return v([c * x for x in self.coor])
 
+    def times_vector(self, ov):
+        assert self.dim == ov.dim, 'Vectors must be the same dimensionality.'
+        return v([x * y for x, y in zip(self.coor, ov.coor)])
+
     def __mul__(self, o):
         if type(o) in [int, float]:
             return self.times_scalar(o)
+        elif type(o) is v:
+            return self.times_vector(o)
         else:
-            raise TypeError('Vectors can only be multiplied by a number.')
+            return NotImplemented
+
+    def __rmul__(self, o):
+        return self.__mul__(o)
