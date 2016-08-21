b5ad6d750f056be11c915d2b6b30fbf78e5538f8
Mikel Bober-Irizar <mikel@mxbi.net>
2016-08-21 14:07:50 +0200

    Implement reverse subtraction

--- vec.py
+++ vec.py
@@ -3,7 +3,7 @@ class v(object):
         try:
             if not coor:
                 raise ValueError
-            self.coor = tuple(coor)
+            self.coor = list(coor)
             self.dim = len(coor)
 
         except KeyboardInterrupt:
@@ -13,9 +13,11 @@ class v(object):
         except TypeError:
             raise TypeError('The co-ordinates must be an iterable')
 
+    # Called when converting to string or printing
     def __str__(self):
         return 'Vector: ' + str(self.coor)
 
+    # Checking for equality
     def __eq__(self, ov):
         return self.coor == ov.coor
 
@@ -43,21 +45,33 @@ class v(object):
 
     def minus_vector(self, ov):
         assert self.dim == ov.dim, 'Vectors must be the same dimensionality.'
-        return v([x + y for x, y in zip(self.coor, ov.coor)])
+        return v([x - y for x, y in zip(self.coor, ov.coor)])
 
     def minus_scalar(self, c):
         return v([x - c for x in self.coor])
 
+    def rminus_vector(self, ov):
+        assert self.dim == ov.dim, 'Vectors must be the same dimensionality.'
+        return v([y - x for x, y in zip(self.coor, ov.coor)])
+
+    def rminus_scalar(self, c):
+        return v([c - x for x in self.coor])
+
     def __sub__(self, o):
         if type(o) in [int, float]:
-            return self.plus_scalar(o)
+            return self.minus_scalar(o)
         elif type(o) is v:
-            return self.plus_vector(o)
+            return self.minus_vector(o)
         else:
             return NotImplemented
 
     def __rsub__(self, o):
-        return self.__sub__(o)
+        if type(o) in [int, float]:
+            return self.minus_scalar(o)
+        elif type(o) is v:
+            return self.minus_vector(o)
+        else:
+            return NotImplemented
 
     # Multiplication
 
